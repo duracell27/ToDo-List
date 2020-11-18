a0024faac29286e1e44990dd6e8f482568ef2f62
duracell27 <volodymyrshmidt@gmail.com>
2020-11-18 13:15:21 +0200

    second video ready

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const input = document.querySelector('#addNewTask');
 const list = document.querySelector('#list-group');
 const emptylist = document.querySelector('#empty-list-item');
 
-form.addEventListener('submit', function (e) {
+form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     const inputText = input.value;
@@ -21,10 +21,23 @@ form.addEventListener('submit', function (e) {
     input.focus();
 });
 
-function checkEmptyList (){
-    if(list.children.length > 1){
+list.addEventListener('click', function(e) {
+    if (e.target.getAttribute('data-action') == 'delete-task') {
+        e.target.closest('.list-group-item').remove();
+        checkEmptyList();
+    } else if (e.target.getAttribute('data-action') == 'ready') {
+        const targetSpan = e.target.closest('.list-group-item');
+        targetSpan.querySelector('.task-title').classList.add('task-title--done');
+        targetSpan.querySelector('.task-title').setAttribute('contenteditable', 'false');
+        list.insertAdjacentElement('beforeend', targetSpan);
+        targetSpan.querySelector('button[data-action="ready"]').remove();
+    }
+})
+
+function checkEmptyList() {
+    if (list.children.length > 1) {
         emptylist.style.display = 'none';
-    }else{
+    } else {
         emptylist.style.display = 'block';
     }
 }
